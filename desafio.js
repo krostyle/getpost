39653c0b210245fadf2e44b0fed9f3897501e3c9
Krostyle <diegoo.albarran@gmail.com>
2021-06-16 02:07:24 -0400

    Se sube proyecto finalizado

---
+++ desafio.js
@@ -0,0 +1,32 @@
+const URL = 'https://jsonplaceholder.typicode.com/postsas'
+const postData = document.getElementById('post-data')
+const request = async() => {
+    try {
+        const res = await fetch(URL);
+        const data = await res.json();
+        createPost(data);
+    } catch (error) {
+        console.error(error);
+    }
+
+}
+
+const createPost = (data) => {
+    postData.innerHTML = ''
+    postData.innerHTML += /*HTML*/
+        `
+        <ul id ='list'>
+        </ul>`;
+    const list = document.getElementById('list')
+    data.forEach(element => {
+        list.innerHTML += /*HTML*/
+            `<li><strong>${element.title}</strong>
+                <p>${element.body}</p>    
+            </li>
+        `
+    });
+}
+
+const getPosts = async() => {
+    await request();
+}
